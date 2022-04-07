08887f77f3ed742ab7fef7ef57f5c8632d790a4e
Iain Smith <b099l3@gmail.com>
2022-04-07 18:10:47 +0100

    remove auth session - i dont think i need this?

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,4 @@
 import { useAnalytics } from 'lib/analytics';
-import { SessionProvider } from 'next-auth/react';
 import { ThemeProvider } from 'next-themes';
 import type { AppProps } from 'next/app';
 import 'styles/global.css';
@@ -9,10 +8,8 @@ export default function App({ Component, pageProps }: AppProps) {
   useAnalytics();
 
   return (
-    <SessionProvider session={pageProps.session}>
       <ThemeProvider attribute="class">
         <Component {...pageProps} />
       </ThemeProvider>
-    </SessionProvider>
   );
 }
