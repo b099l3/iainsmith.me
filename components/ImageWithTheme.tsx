69be63bd8fcfd2b5e4480f07a86024781d3d041c
Iain Smith <b099l3@gmail.com>
2022-04-23 12:18:54 +0100

    fix for theme being 'system' on load

--- components/ImageWithTheme.tsx
+++ components/ImageWithTheme.tsx
@@ -2,12 +2,12 @@ import { useTheme } from 'next-themes';
 import Image from 'next/image';
 
 export default function ImageWithTheme(props) {
-  const { theme } = useTheme();
+  const { resolvedTheme } = useTheme();
 
   return (
     <Image
       alt={props.alt}
-      src={theme === 'light' ? props.light : props.dark}
+      src={resolvedTheme === 'light' ? props.light : props.dark}
       {...props}
     />
   );
