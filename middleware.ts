e0de5c5061473c84932089635187729ee788f2c6
Iain Smith <b099l3@gmail.com>
2022-09-12 13:47:56 +0100

    think i broke the middleware

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,7 @@
-import type { NextFetchEvent, NextRequest } from 'next/server';
+import type { NextRequest } from 'next/server';
 import { NextResponse } from 'next/server';
 
-export function middleware(req: NextRequest, ev: NextFetchEvent) {
+export function middleware(req: NextRequest) {
   const ContentSecurityPolicy = `
     default-src 'self';
     script-src 'self' 'unsafe-eval' 'unsafe-inline' *.youtube.com *.twitter.com *.googletagmanager.com;
