89a0057f07c5a273dc42eeeb7006b5dd223c52b1
Iain Smith <b099l3@gmail.com>
2022-09-14 01:52:07 +0100

    removing preact

--- pages/blog.tsx
+++ pages/blog.tsx
@@ -4,7 +4,7 @@ import TopBlogPosts from 'components/TopBlogPosts';
 import { allBlogs } from 'contentlayer/generated';
 import { pick } from 'lib/utils';
 import { InferGetStaticPropsType } from 'next';
-import { useState } from 'react';
+import { Suspense, useState } from 'react';
 
 export default function Blog({
   posts
@@ -17,63 +17,65 @@ export default function Blog({
   );
 
   return (
-    <Container
-      title="Blog – Iain Smith"
-      description="Thoughts on the software industry, programming, tech, videography, music, and my personal life."
-    >
-      <div className="flex flex-col items-start justify-center max-w-2xl mx-auto mb-16">
-        <h1 className="mb-4 text-3xl font-bold tracking-tight text-black md:text-5xl dark:text-white">
-          Blog
-        </h1>
-        <p className="mb-4 text-gray-600 dark:text-gray-400">
-          {`Hey! I'm Iain. Welcome to my blog. Here's every post I've ever written, mostly about mobile development.
-            In total, I've written ${posts.length} articles on my blog. I write to share what I learned.
-            Use the search below to filter by title, category or sub text.`}
-        </p>
-        <div className="relative w-full mb-4">
-          <input
-            aria-label="Search articles"
-            type="text"
-            onChange={(e) => setSearchValue(e.target.value)}
-            placeholder="Search articles"
-            className="block w-full px-4 py-2 text-gray-900 bg-white border border-gray-200 rounded-md dark:border-gray-900 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-800 dark:text-gray-100"
-          />
-          <svg
-            className="absolute w-5 h-5 text-gray-400 right-3 top-3 dark:text-gray-300"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth={2}
-              d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-            />
-          </svg>
-        </div>
-        {!searchValue && (
-          <>
-            <h3 className="mt-8 mb-4 text-2xl font-bold text-transparent md:text-4xl bg-clip-text bg-gradient-to-r from-indigo-500 via-teal-500 to-indigo-500 animate-text">
-              Most Popular
-            </h3>
-            <TopBlogPosts />
-          </>
-        )}
-        <h3 className="mt-8 mb-4 text-2xl font-bold tracking-tight text-black md:text-4xl dark:text-white">
-          All Posts
-        </h3>
-        {!filteredBlogPosts.length && (
+    <Suspense fallback={null}>
+      <Container
+        title="Blog – Iain Smith"
+        description="Thoughts on the software industry, programming, tech, videography, music, and my personal life."
+      >
+        <div className="flex flex-col items-start justify-center max-w-2xl mx-auto mb-16">
+          <h1 className="mb-4 text-3xl font-bold tracking-tight text-black md:text-5xl dark:text-white">
+            Blog
+          </h1>
           <p className="mb-4 text-gray-600 dark:text-gray-400">
-            No posts found.
+            {`Hey! I'm Iain. Welcome to my blog. Here's every post I've ever written, mostly about mobile development.
+              In total, I've written ${posts.length} articles on my blog. I write to share what I learned.
+              Use the search below to filter by title, category or sub text.`}
           </p>
-        )}
-        {filteredBlogPosts.map((post) => (
-          <BlogPost key={post.title} {...post} />
-        ))}
-      </div>
-    </Container>
+          <div className="relative w-full mb-4">
+            <input
+              aria-label="Search articles"
+              type="text"
+              onChange={(e) => setSearchValue(e.target.value)}
+              placeholder="Search articles"
+              className="block w-full px-4 py-2 text-gray-900 bg-white border border-gray-200 rounded-md dark:border-gray-900 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-800 dark:text-gray-100"
+            />
+            <svg
+              className="absolute w-5 h-5 text-gray-400 right-3 top-3 dark:text-gray-300"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+              />
+            </svg>
+          </div>
+          {!searchValue && (
+            <>
+              <h3 className="mt-8 mb-4 text-2xl font-bold text-transparent md:text-4xl bg-clip-text bg-gradient-to-r from-indigo-500 via-teal-500 to-indigo-500 animate-text">
+                Most Popular
+              </h3>
+              <TopBlogPosts />
+            </>
+          )}
+          <h3 className="mt-8 mb-4 text-2xl font-bold tracking-tight text-black md:text-4xl dark:text-white">
+            All Posts
+          </h3>
+          {!filteredBlogPosts.length && (
+            <p className="mb-4 text-gray-600 dark:text-gray-400">
+              No posts found.
+            </p>
+          )}
+          {filteredBlogPosts.map((post) => (
+            <BlogPost key={post.title} {...post} />
+          ))}
+        </div>
+      </Container>
+      </Suspense>
   );
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,5 @@
+import { Suspense } from 'react';
+
 import Link from 'next/link';
 import BlogPostCard from '../components/BlogPostCard';
 import Container from '../components/Container';
@@ -7,71 +9,73 @@ import Subscribe from '../components/Subscribe';
 
 export default function Home() {
   return (
-    <Container>
-      <div className="flex flex-col items-start justify-center max-w-2xl pb-16 mx-auto border-gray-200 dark:border-gray-700">
-        <div className="flex flex-col-reverse items-start sm:flex-row">
-          <div className="flex flex-col pr-8">
-          <ImageWithTheme
-            alt={`Iain Smith`}
-            light={`/logo-light.svg`}
-            dark={`/logo-dark.svg`}
-            width={1627 / 2}
-            height={700 / 2}
-          />
+    <Suspense fallback={null}>
+      <Container>
+        <div className="flex flex-col items-start justify-center max-w-2xl pb-16 mx-auto border-gray-200 dark:border-gray-700">
+          <div className="flex flex-col-reverse items-start sm:flex-row">
+            <div className="flex flex-col pr-8">
+            <ImageWithTheme
+              alt={`Iain Smith`}
+              light={`/logo-light.svg`}
+              dark={`/logo-dark.svg`}
+              width={1627 / 2}
+              height={700 / 2}
+            />
 
-        <h2 className="mb-12 tracking-tight text-center text-black text-l md:text-xl dark:text-white">
-            Helping developers build apps efficiently. 
-            </h2>
+          <h2 className="mb-12 tracking-tight text-center text-black text-l md:text-xl dark:text-white">
+              Helping developers build apps efficiently. 
+              </h2>
+            </div>
           </div>
+          <h3 className="mb-3 text-2xl font-bold tracking-tight text-black md:text-4xl dark:text-white">
+            Featured Posts
+          </h3>
+          <h2 className="tracking-tight text-black mb-9 text-l md:text-xl dark:text-white">
+              Blogging about mobile development, productivity, and Flutter.
+              </h2>
+          <div className="flex flex-col gap-6 md:flex-row">
+            <BlogPostCard
+              title="The future-proof solution to manage your Flutter versions: global, FVM, or asdf-vm?"
+              category="Flutter"
+              slug="future-proof-your-flutter-env"
+              gradient="from-[#D8B4FE] to-[#818CF8]"
+            />
+            <BlogPostCard
+              title="Flutter Hackathon #Hack20"
+              category="Flutter"
+              slug="flutter-hackathon-hack20"
+              gradient="from-[#6EE7B7] via-[#3B82F6] to-[#9333EA]"
+            />
+            <BlogPostCard
+              title="Flutter - a modern declarative UI toolkit"
+              category="Flutter"
+              slug="flutter-declarative-ui-toolkit"
+              gradient="from-[#FDE68A] via-[#FCA5A5] to-[#FECACA]"
+            />
+          </div>
+          <Link href="/blog">
+            <a className="flex h-6 mt-8 leading-7 text-gray-600 transition-all rounded-lg dark:text-gray-400 hover:text-gray-800 dark:hover:text-gray-200">
+              Read all posts
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                className="w-6 h-6 ml-1"
+              >
+                <path
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M17.5 12h-15m11.667-4l3.333 4-3.333-4zm3.333 4l-3.333 4 3.333-4z"
+                />
+              </svg>
+            </a>
+          </Link>
+          <span className="h-16" />
+          <Subscribe />
         </div>
-        <h3 className="mb-3 text-2xl font-bold tracking-tight text-black md:text-4xl dark:text-white">
-          Featured Posts
-        </h3>
-        <h2 className="tracking-tight text-black mb-9 text-l md:text-xl dark:text-white">
-            Blogging about mobile development, productivity, and Flutter.
-            </h2>
-        <div className="flex flex-col gap-6 md:flex-row">
-          <BlogPostCard
-            title="The future-proof solution to manage your Flutter versions: global, FVM, or asdf-vm?"
-            category="Flutter"
-            slug="future-proof-your-flutter-env"
-            gradient="from-[#D8B4FE] to-[#818CF8]"
-          />
-          <BlogPostCard
-            title="Flutter Hackathon #Hack20"
-            category="Flutter"
-            slug="flutter-hackathon-hack20"
-            gradient="from-[#6EE7B7] via-[#3B82F6] to-[#9333EA]"
-          />
-          <BlogPostCard
-            title="Flutter - a modern declarative UI toolkit"
-            category="Flutter"
-            slug="flutter-declarative-ui-toolkit"
-            gradient="from-[#FDE68A] via-[#FCA5A5] to-[#FECACA]"
-          />
-        </div>
-        <Link href="/blog">
-          <a className="flex h-6 mt-8 leading-7 text-gray-600 transition-all rounded-lg dark:text-gray-400 hover:text-gray-800 dark:hover:text-gray-200">
-            Read all posts
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              className="w-6 h-6 ml-1"
-            >
-              <path
-                stroke="currentColor"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="M17.5 12h-15m11.667-4l3.333 4-3.333-4zm3.333 4l-3.333 4 3.333-4z"
-              />
-            </svg>
-          </a>
-        </Link>
-        <span className="h-16" />
-        <Subscribe />
-      </div>
-    </Container>
+      </Container>
+    </Suspense>
   );
 }
