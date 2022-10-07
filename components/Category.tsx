a9cfe79f38d15978a1d74ff30194b0af6ed3abe0
Iain Smith <b099l3@gmail.com>
2022-10-07 20:59:49 +0100

    removed animation from category chip

--- components/Category.tsx
+++ components/Category.tsx
@@ -6,13 +6,13 @@ export default function Category({category}) {
   switch (category) {
     case 'Flutter':
       return (
-        <span className="flex px-4 py-1 text-xs font-semibold text-white rounded-full cursor-pointer bg-gradient-to-tr from-flutter-blue-500 to-sky-500 animate-text align-center w-max">
+        <span className="flex px-4 py-1 text-xs font-semibold text-white rounded-full cursor-pointer bg-gradient-to-tr from-flutter-blue-500 to-sky-500 align-center w-max">
           {category}
         </span>
       );
     default:
       return (
-        <span className="flex px-4 py-1 text-xs font-semibold text-white rounded-full cursor-pointer bg-gradient-to-tr from-gray-900 to-gray-100 animate-text align-center w-max">
+        <span className="flex px-4 py-1 text-xs font-semibold text-white rounded-full cursor-pointer bg-gradient-to-tr from-gray-900 to-gray-100 align-center w-max">
           {category}
         </span>
       );
