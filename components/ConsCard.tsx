a83a1746faf8b9ba03636bb575a3be950335ac8e
Iain Smith <b099l3@gmail.com>
2023-10-29 17:14:42 +0000

    Fixing pros/cons components

--- components/ConsCard.tsx
+++ components/ConsCard.tsx
@@ -1,20 +1,19 @@
-export default function ConsCard({ title, cons }) {
+
+export default function es({ title, children }) {
   return (
-    <div className="w-full p-6 my-6 border border-red-200 dark:border-red-900 bg-red-50 dark:bg-red-900/50 rounded-xl">
-      <span>{title}</span>
-      <div className="mt-4">
-        {cons.map((con) => (
-          <div key={con} className="flex items-center mb-2 font-medium">
-            <div className="w-4 h-4 mr-2">
-              <svg className="w-4 h-4 text-red-500" viewBox="0 0 24 24">
-                <g fill="currentColor" >
+    <div className="w-full p-6 my-4 border-2 border-red-200 dark:border-red-900 bg-red-50 dark:bg-red-900/50 rounded-xl">
+      <div className="items-center mb-2 font-medium">
+        <div className="flex items-center">
+          <div className="mr-3 align-middle w-7 h-7">
+            <svg className="text-red-500 w-7 h-7" viewBox="0 0 24 24">
+              <g fill="currentColor">
                 <path d="M12 2c5.514 0 10 4.486 10 10s-4.486 10-10 10-10-4.486-10-10 4.486-10 10-10zm0-2c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm6 16.538l-4.592-4.548 4.546-4.587-1.416-1.403-4.545 4.589-4.588-4.543-1.405 1.405 4.593 4.552-4.547 4.592 1.405 1.405 4.555-4.596 4.591 4.55 1.403-1.416z"/>
-                </g>
-              </svg>
-            </div>
-            <span>{con}</span>
+              </g>
+            </svg>
           </div>
-        ))}
+          <div className="text-lg">{title}</div>
+        </div>
+        {children}
       </div>
     </div>
   );
--- components/ProsCard.tsx
+++ components/ProsCard.tsx
@@ -1,20 +1,18 @@
-export default function ProsCard({ title, pros }) {
+export default function es({ title, children }) {
   return (
-    <div className="w-full p-6 my-4 border border-green-200 dark:border-green-900 bg-green-50 dark:bg-green-900/50 rounded-xl">
-      <span>{title}</span>
-      <div className="mt-4">
-        {pros.map((pro) => (
-          <div key={pro} className="flex items-center mb-2 font-medium">
-            <div className="w-4 h-4 mr-2">
-              <svg className="w-4 h-4 text-green-500" viewBox="0 0 24 24">
-                <g fill="currentColor">
-                  <path d="M12 2c5.514 0 10 4.486 10 10s-4.486 10-10 10-10-4.486-10-10 4.486-10 10-10zm0-2c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm-1.959 17l-4.5-4.319 1.395-1.435 3.08 2.937 7.021-7.183 1.422 1.409-8.418 8.591z"/>
-                </g>
-              </svg>
-            </div>
-            <span>{pro}</span>
+    <div className="w-full p-6 my-4 border-2 border-green-200 dark:border-green-900 bg-green-50 dark:bg-green-900/50 rounded-xl">
+      <div className="items-center mb-2 font-medium">
+        <div className="flex items-center">
+          <div className="mr-3 align-middle w-7 h-7">
+            <svg className="text-green-500 w-7 h-7" viewBox="0 0 24 24">
+              <g fill="currentColor">
+                <path d="M12 2c5.514 0 10 4.486 10 10s-4.486 10-10 10-10-4.486-10-10 4.486-10 10-10zm0-2c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm-1.959 17l-4.5-4.319 1.395-1.435 3.08 2.937 7.021-7.183 1.422 1.409-8.418 8.591z"/>
+              </g>
+            </svg>
           </div>
-        ))}
+          <div className="text-lg">{title}</div>
+        </div>
+        {children}
       </div>
     </div>
   );
